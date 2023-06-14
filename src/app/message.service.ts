09144403ad65415fbde6179a33250593f6e6f759
pwang009 <pwang009@hotmail.com>
2023-06-14 13:41:42 -0700

    some clean-up

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -14,10 +14,3 @@ export class MessageService {
     this.messages = [];
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
